3aacdab652b5f514c2c586a0bc96daacfaa2919a
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-11-19 20:20:11 +0500

    Task2

---
+++ Tyuiu.KomarovMA.Sprint5.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMA.Sprint5.Task2.V27.Lib;
+using System.IO;
+namespace Tyuiu.KomarovMA.Sprint5.Task2.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"D:\SOURSE\Tyuiu.KomarovMA.Sprint5\Tyuiu.KomarovMA.Sprint5.Task0.V10\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint5.Task2.V27/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMA.Sprint5.Task2.V27.Lib;
+namespace Tyuiu.KomarovMA.Sprint5.Task2.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { { 1, 4, 3 },
+                                          { 1, 1, 4 },
+                                         { 4, 3, 8 } };
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Комаров М.А. | СМАРТб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Класс File. Запись данных в текстовый файл                                   *");
+            Console.WriteLine("* Задание #2                                                                   *");
+            Console.WriteLine("* Вариант #27                                                                  *");
+            Console.WriteLine("* Выполнил: Комаров Максим Александрович | СМАРТб-23-1                         *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("*Дан двумерный целочисленный массив 3 на 3 элементов, заполненный значениями   *");
+            Console.WriteLine("* с клавиатуры. Заменить нечетные элементы массива на 0.                       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.         *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Массив:");
+            Console.WriteLine("Введите значения для массива:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}

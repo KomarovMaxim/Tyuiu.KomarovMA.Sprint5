28920aafadf62ef16e247998fd1541f7b4c11ac8
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-11-19 20:49:52 +0500

    Task3

---
+++ Tyuiu.KomarovMA.Sprint5.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.KomarovMA.Sprint5.Task3.V3.Lib
+{
+    public class DataService : ISprint5Task3V3
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+
+            double z = x * Math.Sqrt(x+3);
+
+            z = Math.Round(z, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(z));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint5.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMA.Sprint5.Task3.V3.Lib;
+using System.IO;
+namespace Tyuiu.KomarovMA.Sprint5.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"D:\SOURSE\Tyuiu.KomarovMA.Sprint5\Tyuiu.KomarovMA.Sprint5.Task0.V10\bin\Debug\";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}

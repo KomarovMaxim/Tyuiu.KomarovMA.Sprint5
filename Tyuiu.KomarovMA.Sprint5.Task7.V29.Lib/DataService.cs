5792598ef4916e0a64b6bf7cc290e393b554f270
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-11-26 20:57:20 +0500

    Task7

---
+++ Tyuiu.KomarovMA.Sprint5.Task7.V29.Lib/DataService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.KomarovMA.Sprint5.Task7.V29.Lib
+{
+    public class DataService : ISprint5Task7V29
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\Users\masan\SHARP\OutPutDataFileTask7V29.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ');
+                    foreach (string word in words)
+                    {
+                        if (string.IsNullOrWhiteSpace(word))
+                        {
+                            if (int.TryParse(word, out int _))
+                            {
+                                if (word.Length > 1)
+                                {
+                                    File.AppendAllText(pathSaveFile, word + " ");
+                                }
+                            }
+                            else
+                            {
+                                File.AppendAllText(pathSaveFile, word + " ");
+                            }
+                        }
+                    }
+                    File.AppendAllText(pathSaveFile, Environment.NewLine);
+                }
+            }
+
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint5.Task7.V29/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMA.Sprint5.Task7.V29.Lib;
+using System.IO;
+namespace Tyuiu.KomarovMA.Sprint5.Task7.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Кондраков А.А. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый фай                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Кондраков Александр Артемович | СМАРТб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadDataAndSave(@"C:\Users\masan\SHARP\InPutDataFileTask7V29.txt"));
+            Console.ReadKey();
+        }
+    }
+}

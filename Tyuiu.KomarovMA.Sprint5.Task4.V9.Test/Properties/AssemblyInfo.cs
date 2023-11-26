7e94993c3c2509e5a70917cdab9fcd0b9b512a84
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-11-26 15:14:18 +0500

    Task4

---
+++ Tyuiu.KomarovMA.Sprint5.Task4.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomarovMA.Sprint5.Task4.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomarovMA.Sprint5.Task4.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c1795c9-f3c0-4583-a9e0-d4c2cbce8e7d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KomarovMA.Sprint5.Task4.V9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMA.Sprint5.Task4.V9.Lib;
+namespace Tyuiu.KomarovMA.Sprint5.Task4.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Комаров М.А. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Комаров Максим Александрович | СМАРТб-23-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(@"C:\Users\masan\SHARP\InPutDataFileTask4V9.txt"));
+            Console.ReadKey();
+
+        }
+    }
+}
